883fb99bac96f156dfb7d9a22f0618d707c68388
Jamie Ellis <Jamie.Ellis@softwire.com>
2017-07-12 14:37:19 +0100

    Fixed book class to include author

--- Bookish/Bookish.DataAccess/Book.cs
+++ Bookish/Bookish.DataAccess/Book.cs
@@ -10,11 +10,11 @@ namespace Bookish.DataAccess
     {
         public string ISBN { get; set; }
         public string Title { get; set; }
-        public string Author { get; set; }
+        public string Name { get; set; }
 
         public override string ToString()
         {
-            return $"{ISBN}\t{Title}\t{Author}";
+            return $"{ISBN}\t{Title}\t{Name}";
         }
     }
 }
